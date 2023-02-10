82ceb8afb84601fb104cdba3863b6356e2bb52a5
Trent Osterman <trentosterman@gmail.com>
2023-02-10 10:27:11 -0600

    Completed the task of making functions to add items to an array, to list the array items, and to empty the array.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem (string) {
+    item = basket.push(string);
+    return true;
+};
+console.log(addItem('fruit'));
+console.log(basket);
+console.log(addItem('basketballs'));
+console.log(basket);
+console.log(addItem('movies'));
+console.log(basket);
+
+function listItems (array) {
+     array = basket
+    for (listIndex = 0; listIndex<basket.length; listIndex++) {
+        console.log(array[listIndex]);
+    } return basket;
+};
+listItems();
+
+function empty (clear) {
+    basket = [];
+    return basket;
+};
+empty();
+console.log(basket);
+
