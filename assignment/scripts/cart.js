575c97d76c2523df10a996f33642b067a6ecf19e
Trent Osterman <trentosterman@gmail.com>
2023-02-10 10:56:40 -0600

    Answered the stretch goals to make functions to set the max length of the basket array and a function to remove an item from the array.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,13 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
-function addItem (string) {
-    item = basket.push(string);
-    return true;
+function addItem(string) {
+    if (isFull) {
+        item = basket.push(string);
+        return true;
+    } else if (isFull) {
+        return false;
+    };
 };
 console.log(addItem('fruit'));
 console.log(basket);
@@ -12,16 +16,19 @@ console.log(addItem('basketballs'));
 console.log(basket);
 console.log(addItem('movies'));
 console.log(basket);
+addItem('dog');
+addItem('cats')
+addItem('food');
 
-function listItems (array) {
-     array = basket
-    for (listIndex = 0; listIndex<basket.length; listIndex++) {
+function listItems(array) {
+    array = basket
+    for (listIndex = 0; listIndex < basket.length; listIndex++) {
         console.log(array[listIndex]);
     } return basket;
 };
 listItems();
 
-function empty (clear) {
+function empty(clear) {
     basket = [];
     return basket;
 };
@@ -29,3 +36,16 @@ empty();
 console.log(basket);
 
 const maxItems = 5
+function isFull(array) {
+    if (basket.length < maxItems) {
+        return false;
+    } return true;
+};
+
+function removeItem (string) {
+    if (basket.indexof(string) = string) {
+        basket.splice();
+        return string;
+    } return null;
+};
+removeItem('food');
